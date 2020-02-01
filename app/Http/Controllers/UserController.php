27f4fc746d2b07e424070bb675d9509f869b2aee
Fynas1403 <malfynas_putra_27rpl@student.smktelkom-mlg.sch.id>
2020-02-02 05:06:28 +0700

    Authentication jwt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\User2;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Petugas_model;
+use Auth;
 
 class UserController extends Controller
 {
@@ -64,6 +67,140 @@ class UserController extends Controller
         return response()->json(compact('user','token'),201);
     }
 
+    public function update($id_petugas,Request $req)
+    {
+        $validator=Validator::make($req->all(),
+        [
+            'nama_petugas'=>'required',
+            'alamat'=>'required',
+            'telp'=>'required',
+            'username'=>'required',
+            'password'=>'required',
+            'level'=>'required'
+        ]);
+
+        if($validator->fails()){
+            return Response()->json($validator->errors());
+        }
+        $ubah=Petugas_model::where('id', $id_petugas)->update([
+            'nama_petugas'=>$req->nama_petugas,
+            'alamat'=>$req->alamat,
+            'telp'=>$req->telp,
+            'username'=>$req->username,
+            'password'=>$req->password,
+            'level'=>$req->level
+        ]);
+        if($ubah){
+            return Response()->json(['status'=>1]);
+        }else{
+            return Response()->json(['status'=>0]);
+        }
+    }
+    
+    public function hapus($id_petugas)
+    {
+        $hapus=Petugas_model::where('id',$id_petugas)->delete();
+        if($hapus){
+            return Response()->json(['status'=>1]);
+        }else{
+            return Response()->json(['status'=>0]);
+        }
+    }
+
+    public function tampil_petugas()
+    {
+        $data_petugas=Petugas_model::get();
+        return Response()->json($data_petugas);
+    }
+
+
+    
+    //////////////////////ANGGOTA
+
+    public function register2(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama_anggota' => 'required|string|max:255',
+            'alamat' => 'required|string|max:255',
+            'telp' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'password' => 'required|string|min:6|confirmed',
+            'level' => 'required|string|max:255',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User2::create([
+            'nama_anggota' => $request->get('nama_anggota'),
+            'alamat' => $request->get('alamat'),
+            'telp' => $request->get('telp'),
+            'username' => $request->get('username'),
+            'password' => Hash::make($request->get('password')),
+            'level' => $request->get('level'),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        $update = User2::where('nama_anggota', $request->nama_anggota)->update([
+            'nama_anggota' => $request->get('nama_anggota'),
+            'alamat' => $request->get('alamat'),
+            'telp' => $request->get('telp'),
+            'username' => $request->get('username'),
+            'password' => Hash::make($request->get('password')),
+            'level' => $request->get('level'),
+        ]);
+
+        return response()->json(compact('user','token'),201);
+    }
+
+    public function update2($id_anggota,Request $req)
+    {
+        $validator=Validator::make($req->all(),
+        [
+            'nama_anggota'=>'required',
+            'alamat'=>'required',
+            'telp'=>'required',
+            'username'=>'required',
+            'password'=>'required',
+            'level'=>'required'
+        ]);
+
+        if($validator->fails()){
+            return Response()->json($validator->errors());
+        }
+        $ubah=User2::where('id', $id_anggota)->update([
+            'nama_anggota'=>$req->nama_anggota,
+            'alamat'=>$req->alamat,
+            'telp'=>$req->telp,
+            'username'=>$req->username,
+            'password'=>$req->password,
+            'level'=>$req->level
+        ]);
+        if($ubah){
+            return Response()->json(['status'=>1]);
+        }else{
+            return Response()->json(['status'=>0]);
+        }
+    }
+    
+    public function hapus2($id_anggota)
+    {
+        $hapus=User2::where('id',$id_anggota)->delete();
+        if($hapus){
+            return Response()->json(['status'=>1]);
+        }else{
+            return Response()->json(['status'=>0]);
+        }
+    }
+
+    public function tampil_anggota()
+    {
+        $data_anggota=User2::get();
+        return Response()->json($data_anggota);
+    }
+    
     public function getAuthenticatedUser()
     {
         try {
